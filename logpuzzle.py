94704e0ca3b0049ff5569a21516a87c159d302af
Amanda Simmons <carpe737diem@gmail.com>
2020-12-15 18:58:59 -0500

    added auth string

--- logpuzzle.py
+++ logpuzzle.py
@@ -13,6 +13,7 @@ Here's what a puzzle URL looks like (spread out onto multiple lines):
 HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
+__author__ = "Amanda Simmons"
 
 import os
 import re
